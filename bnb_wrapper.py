7b91a4a417b149bf4d1269924459b42aa88b803a
mrhuytran <tranquanghuy2602@gmail.com>
2020-01-31 23:39:43 -0800

    setup

--- bnb_wrapper.py
+++ bnb_wrapper.py
@@ -1,9 +1,11 @@
 import requests
 import json
 import pandas as pd
+from pandas import DataFrame as df
 import hmac
+from interval_enums import Interval
 
-class Client:
+class BinanceClient:
     BASE_URL = 'https:/api.binance.com'
     #endpoint = ''
     #params = request_body
@@ -17,14 +19,26 @@ class Client:
             'klines': '/api/v1/klines',
             'price_ticker': '/api/v3/ticker/price',
             '24hr_ticker': '/api/v3/ticker/24hr',
-            'order': '/api/v3/order', 
+            'order': '/api/v3/order',
+            'test_order': '/api/v3/order/test',
             'open_order': '/api/v3/openOrders',          # all open orders
             'all_order': '/api/v3/allOrders',            # all orders: active, cancelled, filler
             'my_trade': '/api/v3/myTrades',              # all trades for a specific symbol on the account
             'recent_trade': '/api/v3/trades'             # recent trades on the market
         }
 
+    def get_klines(self, symbol: str, interval: Interval):
+        params = {
+            'symbol': symbol,
+            'interval': interval.value
+        }
 
+        url = self.base + self.endpoint['klines']
+
+        response = requests.get(url, params=params)
+        data = json.loads(response.text)
+        
+        return data
 
     '''
         sign your request to Binance API
@@ -41,4 +55,9 @@ class Client:
         
         # add your signature to the request body
         params['signature'] = signature.hexdigest()
-    
+
+
+bnb = BinanceClient('', '')
+
+klines = bnb.get_klines('BNBBTC', Interval._5MINUTE)
+print(klines)
---
+++ interval_enums.py
@@ -0,0 +1,19 @@
+import enum
+
+class Interval(enum.Enum):
+
+    _1MINUTE = '1m'
+    _3MINUTE = '3m'
+    _5MINUTE = '5m'
+    _15MINUTE = '15m'
+    _30MINUTE = '30m'
+    _1HOUR = '1h'
+    _2HOUR = '2h'
+    _4HOUR = '4h'
+    _6HOUR = '6h'
+    _8HOUR = '8h'
+    _12HOUR = '12h'
+    _1DAY = '1d'
+    _3DAY = '3d'
+    _1WEEK = '1w'
+    _1MONTH = '1m'
